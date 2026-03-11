ae0dc6f6038b2ebb6b18f1952ecf5c24018d8f2e
lanvi1998 <lanvihuynh79@gmail.com>
2026-03-11 09:28:12 +0700

    deploy react build

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Kết nối MongoDB Atlas
+// React build
+app.use(express.static(path.join(__dirname, "client/build")))
+
 console.log("Connecting to MongoDB...")
 
 mongoose.connect(
@@ -24,10 +26,8 @@ mongoose.connect(
     console.log("MongoDB error:", err)
 })
 
-// API routes
 app.use("/auth", authRoutes)
 
-// API lấy danh sách user
 app.get("/users", async (req,res)=>{
     try{
         const users = await User.find()
@@ -37,15 +37,11 @@ app.get("/users", async (req,res)=>{
     }
 })
 
-// Serve file static từ public
-app.use(express.static(path.join(__dirname,"public")))
-
-// Trang chủ
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"public","login.html"))
+// React routes
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","build","index.html"))
 })
 
-// chạy server
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>{
