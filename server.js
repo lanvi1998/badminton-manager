e0637b1a661dfc91c3746296a052fa0dba88272c
lanvi1998 <lanvihuynh79@gmail.com>
2026-03-12 09:59:16 +0700

    fix index route

--- server.js
+++ server.js
@@ -1,50 +1,31 @@
 const express = require("express")
-const cors = require("cors")
 const mongoose = require("mongoose")
 const path = require("path")
 
 const authRoutes = require("./routes/auth")
-const User = require("./models/User")
 
 const app = express()
 
-app.use(cors())
 app.use(express.json())
+app.use(express.static("public"))
 
-// serve React build
-app.use(express.static(path.join(__dirname, "public")))
-
-console.log("Connecting to MongoDB...")
-
-mongoose.connect(
-"mongodb+srv://lanvihuynh98:Lanvi1905@cluster0.tm10azb.mongodb.net/badminton?retryWrites=true&w=majority"
-)
-.then(()=>{
-    console.log("MongoDB connected")
-})
-.catch(err=>{
-    console.log("MongoDB error:", err)
-})
-
-// API routes
 app.use("/auth", authRoutes)
 
 app.get("/users", async (req,res)=>{
-    try{
-        const users = await User.find()
-        res.json(users)
-    }catch(err){
-        res.status(500).json({error:err.message})
-    }
+  try{
+    const users = await User.find()
+    res.json(users)
+  }catch(err){
+    res.status(500).json({error:err.message})
+  }
 })
 
-// React fallback
-app.use((req,res)=>{
-    res.sendFile(path.join(__dirname,"public","index.html"))
+app.get("/", (req,res)=>{
+  res.sendFile(path.join(__dirname,"public","login.html"))
 })
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>{
-    console.log("Server running on port " + PORT)
+  console.log("Server running on port " + PORT)
 })
