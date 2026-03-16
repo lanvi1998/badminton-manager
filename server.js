836928ac0be10b1a124f78dbaf342f8e8337d76e
lanvi1998 <lanvihuynh79@gmail.com>
2026-03-16 17:22:01 +0700

    fix mongodb connection

--- server.js
+++ server.js
@@ -3,23 +3,28 @@ const mongoose = require("mongoose")
 const path = require("path")
 
 const authRoutes = require("./routes/auth")
+const User = require("./models/User")
 
 const app = express()
 
 app.use(express.json())
 
-// API
+// CONNECT MONGODB
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("MongoDB connected"))
+.catch(err=>console.log(err))
+
 app.use("/auth", authRoutes)
 
 // React build
-app.use(express.static(path.join(__dirname,"client","build")))
+app.use(express.static(path.join(__dirname,"build")))
 
 app.get("*",(req,res)=>{
-res.sendFile(path.join(__dirname,"client","build","index.html"))
+res.sendFile(path.join(__dirname,"build","index.html"))
 })
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>{
-console.log("Server running on port " + PORT)
+console.log("Server running on port "+PORT)
 })
