c8f93f6015a5481e7a0f1c4f576e3dfff9cd4161
lanvi1998 <lanvihuynh79@gmail.com>
2026-03-16 17:34:02 +0700

    fix mongodb connection

--- server.js
+++ server.js
@@ -9,22 +9,38 @@ const app = express()
 
 app.use(express.json())
 
-// CONNECT MONGODB
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB connected"))
-.catch(err=>console.log(err))
+// MongoDB connection
+mongoose.connect("mongodb+srv://lanvihuynh98:Lanvi1905@cluster0.tm10azb.mongodb.net/badminton?retryWrites=true&w=majority")
+.then(() => {
+  console.log("MongoDB connected")
+})
+.catch((err) => {
+  console.log("MongoDB error:", err)
+})
 
+// serve React build
+app.use(express.static(path.join(__dirname, "build")))
+
+// API routes
 app.use("/auth", authRoutes)
 
-// React build
-app.use(express.static(path.join(__dirname,"build")))
+// test users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
 
-app.get("*",(req,res)=>{
-res.sendFile(path.join(__dirname,"build","index.html"))
+// React router fix
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"))
 })
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,()=>{
-console.log("Server running on port "+PORT)
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT)
 })
